c13f2ccda8f27a64bb3046e0aa119e66e98ade5a
Sush Singh <sushantsingh4720@gmail.com>
2022-08-13 16:19:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
+  
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
@@ -27,20 +22,80 @@ const DUMMY_EXPENSES = [
     id: "e5",
     title: "New Laptop",
     amount: 775,
-    date: new Date(2022,8,29),
+    date: new Date(2022, 8, 29),
   },
   {
     id: "e6",
     title: "Carry Bag",
     amount: 75,
-    date: new Date(2022,10,15),
+    date: new Date(2022, 10, 15),
   },
   {
     id: "e7",
     title: "New Laptop",
     amount: 375,
-    date: new Date(2022,2,10),
-  }
+    date: new Date(2022, 2, 10),
+  },
+  {
+    id: "e8",
+    title: "biscuit ",
+    amount: 35,
+    date: new Date(2019, 6, 5),
+  },
+  {
+    id: "e9",
+    title: "Bike Cover",
+    amount: 85,
+    date: new Date(2019, 9, 10),
+  },
+  {
+    id: "e10",
+    title: "Duffel Bag",
+    amount: 93,
+    date: new Date(2019, 1, 20),
+  },
+  {
+    id: "e11",
+    title: "NoteBook",
+    amount: 75,
+    date: new Date(2019, 5, 4),
+  },
+  {
+    id: "e12",
+    title: "Study Table",
+    amount: 95,
+    date: new Date(2019, 2, 10),
+  },
+  {
+    id: "e13",
+    title: "Pen",
+    amount: 5,
+    date: new Date(2019, 3, 7),
+  },
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e14",
+    title: "Chocolate",
+    amount: 12,
+    date: new Date(2020, 2, 4),
+  },
+  {
+    id: "e15",
+    title: "Bread",
+    amount: 112,
+    date: new Date(2020, 8, 24),
+  },
+  {
+    id: "e16",
+    title: "Shoes",
+    amount: 120,
+    date: new Date(2020, 1, 20),
+  },
 
 ];
 
